4b21fe7801f9a9bff9fd27ffc08fe1702b7f7e94
Sounak Pal <spal@tavisca.com>
2020-02-26 04:22:11 +0530

    Title and Description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'lit-element helpers',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Front end developer trying to help people with lit-element.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
